493fe764c0f3767eb3da12151b02eac937589549
mktcode <kontakt@markus-kottlaender.de>
2018-06-26 01:53:29 +0200

    fixed overlapping, reduced code

--- src/layouts/parts/homepage/homepage.vue
+++ src/layouts/parts/homepage/homepage.vue
@@ -11,39 +11,16 @@ export default {
   // component name.
   name: 'u-layout-homepage',
 
-  // component data.
-  data () {
-    return {}
-  },
-
-  // props.
-  props: {
-    // enable / disable jumbotron.
-    jumbotron: {
-      type: Boolean,
-      default: true
-    }
-  },
-
   // computed properties.
   computed: {
-
     // map common getters.
     ...mapGetters('common', [
       'isMobile',
       'isDesktop'
     ])
-  },
-
-  // component methods.
-  methods: {
-    //
   }
 }
 </script>
 
 <!-- component template. -->
 <template src="./homepage.pug" lang="pug"></template>
-
-<!-- component styles. -->
-<style src="./homepage.styl" lang="stylus"></style>
