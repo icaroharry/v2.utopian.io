23ba441c5c1efb8625966e8eebbde6e0284d9d42
Diego Hernandes <diego@hernandev.com>
2018-06-26 16:54:12 -0300

    parse steem connect callback helper

---
+++ src/services/steem/connect/parseCallback.js
@@ -0,0 +1,28 @@
+import { get } from 'lodash-es'
+import moment from 'moment/moment'
+
+/**
+ * Parse SteemConnect auth callback data.
+ */
+export const parseSteemConnectCallback = (callbackData) => {
+  // extract access token.
+  const token = get(callbackData, 'access_token', null)
+  // extract username.
+  const username = get(callbackData, 'username', null)
+  // get the expiration ttl (seconds).
+  const ttl = get(callbackData, 'expires_in', null)
+
+  // stop on missing fields.
+  if (!token || !username || !ttl) {
+    throw new Error('Invalid SteemConnect data.')
+  }
+
+  // determine the actual token expiration.
+  const expiration = moment.utc().add(ttl, 'seconds').toISOString()
+
+  // return a normalized SteemConnect data object.
+  return { token, username, ttl, expiration }
+}
+
+// default export of the main function.
+export default parseSteemConnectCallback
