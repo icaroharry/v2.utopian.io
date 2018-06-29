69a3a3380c85a58ed34bcb6994d04218945cad8a
Diego Hernandes <diego@hernandev.com>
2018-06-29 05:38:56 -0300

    github search refactor

--- src/store/contributions/actions.js
+++ src/store/contributions/actions.js
@@ -1,8 +1,35 @@
 // Contributions Store - Actions.
 
 // imports.
-//
+import { get, map } from 'lodash'
+import * as GitHub from '@octokit/rest'
+
+// create a local github client instance.
+const githubClient = new GitHub()
+
+const makeSearchOptions = (query) => ({
+  q: `${query} in:name fork:true`,
+  sort: 'updated',
+  per_page: 5,
+  page: 1
+})
+
+const mapGithubResults = (result) => {
+  return map(get(result, 'data.items'), (item) => {
+    return {
+      id: get(item, 'id', null),
+      label: get(item, 'full_name', null),
+      value: get(item, 'full_name', null),
+      avatar: get(item, 'owner.avatar_url', null)
+    }
+  })
+}
 
 export const loadDrafts = ({ commit, getters, dispatch, rootGetters }) => {
   // console.log(rootGetters)
+}
+
+// Github repository search.
+export const searchGithubRepository = (store, query) => {
+  return githubClient.search.repos(makeSearchOptions(query)).then(mapGithubResults).catch(() => ([]))
 }
