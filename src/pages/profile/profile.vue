bb7c5790b1cb61c1ed2d8854f90fc7f812e74de0
Ícaro Harry <me@icaro.io>
2018-07-03 01:04:14 -0300

    Create parallax header

--- src/pages/profile/profile.vue
+++ src/pages/profile/profile.vue
@@ -2,7 +2,7 @@
 import ULayoutPage from 'src/layouts/parts/page/page'
 import UPostPreview from 'src/components/post-preview/post-preview'
 import { byOrder } from 'src/services/steem/posts'
-import { concat, last, attempt } from 'lodash-es'
+import { concat, last, attempt, get } from 'lodash-es'
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
@@ -25,6 +25,7 @@ export default {
         'avatar',
         'githubGuest'
       ]),
+      userProfile: {},
       userAccount: {},
       contributions: [],
       isMounted: false,
@@ -38,20 +39,36 @@ export default {
       loadFirebaseAccount: 'auth/loadFirebaseAccount',
       loadSteemAccount: 'steem/loadAccount'
     }),
+    factoryProfile (data) {
+      // extract meta information.
+      const meta = JSON.parse(get(data, 'json_metadata', '{}') || '{}')
+      // extract profile from meta.
+      const profile = get(meta, 'profile', {})
+      // assign avatar field on the profile.
+      profile.avatar = get(profile, 'profile_image')
+      // return the prepared object.
+      return profile
+    },
     loadInitial () {
       this.loading = true
       const username = this.$route.params['username']
 
       if (this.isOwnProfile) { // if the profile belongs to the current logged user, fetch data from vuex
         this.userAccount = this.account()
+        this.userProfile = this.userAccount.profile
         this.loading = false
       } else { // if it is not the logged user, try to fetch from firestore
         this.loadFirebaseAccount(username).then((account) => {
-          this.userAccount = account
           if (account === null) { // if the user is not registered on firestore, fetch from the blockchain
-            this.loadSteemAccount(username).then((result) => {
-              console.log(result)
+            this.loadSteemAccount(username).then((account) => {
+              this.userAccount = account
+              this.userProfile = this.factoryProfile(account)
+              this.loading = false
             })
+          } else {
+            this.userAccount = account
+            this.userProfile = account.profile
+            this.loading = false
           }
         })
       }
@@ -68,6 +85,9 @@ export default {
   computed: {
     isOwnProfile () {
       return this.uid() === this.$route.params['username']
+    },
+    coverImage () {
+      return this.userProfile.cover_image || 'https://source.unsplash.com/1600x900/?coding,computer,tech'
     }
   },
   mounted () {
