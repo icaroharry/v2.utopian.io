5f1ed896198b55747f05cc5c356e2bd1ab055cda
tensor-programming <abeltensor@tensor-programming.com>
2018-07-25 07:47:51 -0400

    fix UT-120, UT-131, UT-132

--- src/services/common/markdown/plugins/fix-html-breaks.js
+++ src/services/common/markdown/plugins/fix-html-breaks.js
@@ -5,9 +5,13 @@
 // regex for matching HTML blocks.
 const blockExpression = /^(<(h[0-9]|div|hr|p|center|ul|li|a|b)([^>]+)?>)/gim
 
+// remove </b> tags to ensure parsing of text. 
+const breaksExpresson = /^(<\/ ?b>)+/gim
+
 // fix a block contents.
 const fixBlockState = (state) => {
   // wrap block elements tags with new lines.
+  state.src = state.src.replace(breaksExpresson, (math) => '')
   state.src = state.src.replace(blockExpression, (match) => `${match}\n\n`)
 }
 
