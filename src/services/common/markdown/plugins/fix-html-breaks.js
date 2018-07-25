508f984dd03b6fb43ce40e52763a260d8505a29b
tensor-programming <abeltensor@tensor-programming.com>
2018-07-25 14:02:38 -0400

    cleaup patch

--- src/services/common/markdown/plugins/fix-html-breaks.js
+++ src/services/common/markdown/plugins/fix-html-breaks.js
@@ -3,15 +3,16 @@
  */
 
 // regex for matching HTML blocks.
-const blockExpression = /^(<(h[0-9]|div|hr|p|center|ul|li|a|b)([^>]+)?>)/gim
+const blockExpression = /^(<(h[0-9]|div|hr|p|center|ul|li|a)([^>]+)?>)/gim
 
-// remove </b> tags to ensure parsing of text. 
+// remove </b> tags to ensure parsing of text.
 const breaksExpresson = /^(<\/ ?b>)+/gim
 
 // fix a block contents.
 const fixBlockState = (state) => {
+  // convert </b> tags to empty strings.
+  state.src = state.src.replace(breaksExpresson, (_) => '')
   // wrap block elements tags with new lines.
-  state.src = state.src.replace(breaksExpresson, (match) => '')
   state.src = state.src.replace(blockExpression, (match) => `${match}\n\n`)
 }
 
