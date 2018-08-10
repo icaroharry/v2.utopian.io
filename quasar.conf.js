3da73a0dafb3debcb693bdd053b3d52e7b60be00
Ícaro Harry <me@icaro.io>
2018-08-10 19:26:56 -0300

    Setup different ENV variables for dev vs prod

--- quasar.conf.js
+++ quasar.conf.js
@@ -26,7 +26,15 @@ module.exports = function (ctx) {
     ],
     // build configuration.
     build: {
-      env: {
+      env: ctx.dev ? {
+        SC2_APP: (process.env.SC2_APP_DEV || '"utopian.signin"'),
+        FIREBASE_API_KEY: (process.env.FIREBASE_API_KEY_DEV || 'null'),
+        FIREBASE_PROJECT_ID: (process.env.FIREBASE_PROJECT_ID_DEV || '"utopian-io"'),
+        FIREBASE_AUTH_DOMAIN: (process.env.FIREBASE_AUTH_DOMAIN_DEV || '"auth.utopian.io"'),
+        FIREBASE_MESSAGING_SENDER_ID: (process.env.FIREBASE_MESSAGING_SENDER_ID_DEV || 'null'),
+        FIREBASE_EMULATOR: (process.env.FIREBASE_EMULATOR_DEV || 'null'),
+        STEEM_API: (process.env.STEEM_API_DEV || '"https://api.steemit.com"')
+      } : {
         SC2_APP: (process.env.SC2_APP || '"utopian.signin"'),
         FIREBASE_API_KEY: (process.env.FIREBASE_API_KEY || 'null'),
         FIREBASE_PROJECT_ID: (process.env.FIREBASE_PROJECT_ID || '"utopian-io"'),
