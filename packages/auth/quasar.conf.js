2dcc71c03d766a92f916266cd123f8e5026c7dd0
Ícaro Harry <me@icaro.io>
2018-10-24 17:33:46 -0300

    Auth quasar.conf (#113)
    * Update quasar.conf and package.json
    
    * Add vuelidate plugin
    
    * Add lint config
    
    * Add src-pwa and src-ssr

--- packages/auth/quasar.conf.js
+++ packages/auth/quasar.conf.js
@@ -1,61 +1,68 @@
-// Configuration for your app
+// environment config.
+require('dotenv').config()
 
+// Configuration for your app
 module.exports = function (ctx) {
   return {
+    preFetch: true,
+    supportIE: false,
+    css: ['app.styl'],
     // app plugins (/src/plugins)
+    extras: ['roboto-font', 'mdi'],
     plugins: [
+      'axios',
       'i18n',
-      'axios'
+      'vuelidate'
     ],
-    css: [
-      'app.styl'
-    ],
-    extras: [
-      ctx.theme.mat ? 'roboto-font' : null,
-      'material-icons' // optional, you are not bound to it
-      // 'ionicons',
-      // 'mdi',
-      // 'fontawesome'
-    ],
-    supportIE: true,
     build: {
+      env: {
+        UTOPIAN_API: process.env.UTOPIAN_API,
+        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+        STEEMCONNECT_CLIENT_ID: (process.env.STEEMCONNECT_CLIENT_ID || '"utopian.signin"'),
+        STEEM_API: (process.env.STEEM_API_DEV || '"https://api.steemit.com"')
+      },
       scopeHoisting: true,
-      // vueRouterMode: 'history',
-      // vueCompiler: true,
-      // gzip: true,
-      // analyze: true,
-      // extractCSS: false,
+      vueRouterMode: 'history',
+      useNotifier: false,
+
       extendWebpack (cfg) {
+        // main loader / js config.
         cfg.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /node_modules/
+          exclude: /(node_modules|quasar)/
+        })
+        cfg.module.rules.push({
+          test: /\.pug$/,
+          loader: 'pug-plain-loader'
         })
       }
     },
+    // dev server configuration.
     devServer: {
-      // https: true,
-      // port: 8080,
-      open: true // opens browser window automatically
+      port: 8081,
+      open: false // no auto browser.
     },
     // framework: 'all' --- includes everything; for dev only!
     framework: {
       components: [
-        'QLayout',
-        'QLayoutHeader',
-        'QLayoutDrawer',
-        'QPageContainer',
-        'QPage',
-        'QToolbar',
-        'QToolbarTitle',
         'QBtn',
+        'QField',
         'QIcon',
-        'QList',
-        'QListHeader',
+        'QInput',
         'QItem',
         'QItemMain',
-        'QItemSide'
+        'QItemSide',
+        'QLayout',
+        'QLayoutDrawer',
+        'QLayoutHeader',
+        'QList',
+        'QListHeader',
+        'QPage',
+        'QPageContainer',
+        'QToolbar',
+        'QToolbarTitle'
       ],
       directives: [
         'Ripple'
@@ -64,13 +71,11 @@ module.exports = function (ctx) {
       plugins: [
         'Notify'
       ]
-      // iconSet: ctx.theme.mat ? 'material-icons' : 'ionicons'
-      // i18n: 'de' // Quasar language
     },
     // animations: 'all' --- includes all animations
     animations: [],
     ssr: {
-      pwa: false
+      pwa: true
     },
     pwa: {
       // workboxPluginMode: 'InjectManifest',
@@ -112,31 +117,6 @@ module.exports = function (ctx) {
         ]
       }
     },
-    cordova: {
-      // id: 'org.cordova.quasar.app'
-    },
-    electron: {
-      // bundler: 'builder', // or 'packager'
-      extendWebpack (cfg) {
-        // do something with Electron process Webpack cfg
-      },
-      packager: {
-        // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
-        // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
-
-        // Window only
-        // win32metadata: { ... }
-      },
-      builder: {
-        // https://www.electron.build/configuration/configuration
-
-        // appId: 'quasar-app'
-      }
-    }
+    starterKit: '1.0.2'
   }
 }
